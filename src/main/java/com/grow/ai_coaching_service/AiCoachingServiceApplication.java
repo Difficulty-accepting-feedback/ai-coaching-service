bee27a1fb955e453910a0102630f6d63141baebe
wkdan <jmy8876@gmail.com>
2025-07-01 19:22:48 +0900

    ⚡ initial commit

---
+++ src/main/java/com/grow/ai_coaching_service/AiCoachingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.grow.ai_coaching_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCoachingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiCoachingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grow/ai_coaching_service/AiCoachingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grow.ai_coaching_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCoachingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
